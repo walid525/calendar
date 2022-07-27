317ef463680244937e6533bc16673d6346d169f6
walid ben hassine <your_email_address@example.com>
2022-07-27 14:16:31 +0100

    first commit

---
+++ src/components/Dashboard/Dashborard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from '../Dashborard/Dashboard'; 
+import './App.css';
+
+
+export default function Dashboard() {
+
+  return(
+
+    <h3>Dashboard</h3>
+
+  );
+
+}
---
+++ src/components/Preferences/Preferences.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Preferences from '../Preferences/Preferences';
+
+
+
+export default function Preferences() {
+
+  return(
+
+    <h2>Preferences</h2>
+
+  );
+
+}
